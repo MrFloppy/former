3b81170521608375fe1e2da3aa2a5b62077ce582
Maximilian Sohrt <max@sohrt.de>
2021-09-13 10:43:02 +0200

    Added templating for a basic form and the show how echo works for our purpose

--- src/web/web.go
+++ src/web/web.go
@@ -2,10 +2,22 @@ package web
 
 import (
 	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/http"
 )
+
+//Templating
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func WebPrint() {
 	fmt.Println("We are also on teh web!")
@@ -19,8 +31,15 @@ func RunServer() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	t := &Template{
+		templates: template.Must(template.ParseGlob("../public/views/*.html")),
+	}
+	e.Renderer = t
+
 	// Routes
 	e.GET("/", hello)
+	e.GET("/testmail", testmail)
+	e.POST("/x/*", sendMail)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -29,4 +48,15 @@ func RunServer() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func testmail(c echo.Context) error {
+	return c.Render(http.StatusOK, "testmail", "World")
+}
+
+func sendMail(c echo.Context) error {
+	subject := c.FormValue("subject")
+	message := c.FormValue("message")
+
+	return c.String(http.StatusOK, subject+message)
 }
