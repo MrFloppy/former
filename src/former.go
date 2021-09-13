ca54fb0af0dbe77e61ceac008c915b42bf653220
Maximilian Sohrt <max@sohrt.de>
2021-09-13 09:54:10 +0200

    Stub for using echo as web framework for the backend

---
+++ src/former.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/former/web"
+)
+
+func main() {
+	fmt.Println("Hello world.")
+	web.WebPrint()
+	web.RunServer()
+}
---
+++ src/web/web.go
@@ -0,0 +1,32 @@
+package web
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func WebPrint() {
+	fmt.Println("We are also on teh web!")
+}
+
+func RunServer() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
